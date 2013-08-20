d0dd4e3588222eed0b1eb6e4beb45750fad0a3e9
ndaonguyen <ndnguyen@apcs.vn>
2013-08-20 15:00:31 +0800

    Add 'password' field to registration

--- Tutorial/GeneratedFiles/ui_tutorial.h
+++ Tutorial/GeneratedFiles/ui_tutorial.h
@@ -22,6 +22,7 @@
 #include <QtGui/QMenuBar>
 #include <QtGui/QPushButton>
 #include <QtGui/QStatusBar>
+#include <QtGui/QTabWidget>
 #include <QtGui/QToolBar>
 #include <QtGui/QWidget>
 
@@ -31,17 +32,23 @@ class Ui_TutorialClass
 {
 public:
     QWidget *centralWidget;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QLabel *myLabel;
+    QLabel *imgLabel;
     QWidget *horizontalLayoutWidget;
     QHBoxLayout *horizontalLayout;
     QPushButton *pushButton;
     QPushButton *pushButton_2;
+    QPushButton *fileEdit;
     QLineEdit *nameEdit;
     QLabel *nameLabel;
     QLabel *birthLabel;
-    QLabel *imgLabel;
-    QPushButton *fileEdit;
     QDateEdit *birthdayEdit;
-    QLabel *myLabel;
+    QLabel *passwordLabel;
+    QLineEdit *passwordEdit;
+    QWidget *tab_3;
+    QWidget *tab_2;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -50,12 +57,23 @@ public:
     {
         if (TutorialClass->objectName().isEmpty())
             TutorialClass->setObjectName(QString::fromUtf8("TutorialClass"));
-        TutorialClass->resize(343, 310);
+        TutorialClass->resize(435, 385);
         centralWidget = new QWidget(TutorialClass);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
-        horizontalLayoutWidget = new QWidget(centralWidget);
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
+        tabWidget->setGeometry(QRect(30, 10, 371, 321));
+        tab = new QWidget();
+        tab->setObjectName(QString::fromUtf8("tab"));
+        myLabel = new QLabel(tab);
+        myLabel->setObjectName(QString::fromUtf8("myLabel"));
+        myLabel->setGeometry(QRect(90, 150, 101, 91));
+        imgLabel = new QLabel(tab);
+        imgLabel->setObjectName(QString::fromUtf8("imgLabel"));
+        imgLabel->setGeometry(QRect(80, 110, 46, 13));
+        horizontalLayoutWidget = new QWidget(tab);
         horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
-        horizontalLayoutWidget->setGeometry(QRect(50, 220, 241, 31));
+        horizontalLayoutWidget->setGeometry(QRect(50, 260, 241, 31));
         horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
         horizontalLayout->setSpacing(6);
         horizontalLayout->setContentsMargins(11, 11, 11, 11);
@@ -71,31 +89,38 @@ public:
 
         horizontalLayout->addWidget(pushButton_2);
 
-        nameEdit = new QLineEdit(centralWidget);
+        fileEdit = new QPushButton(tab);
+        fileEdit->setObjectName(QString::fromUtf8("fileEdit"));
+        fileEdit->setGeometry(QRect(150, 110, 111, 23));
+        nameEdit = new QLineEdit(tab);
         nameEdit->setObjectName(QString::fromUtf8("nameEdit"));
         nameEdit->setGeometry(QRect(150, 20, 113, 20));
-        nameLabel = new QLabel(centralWidget);
+        nameLabel = new QLabel(tab);
         nameLabel->setObjectName(QString::fromUtf8("nameLabel"));
         nameLabel->setGeometry(QRect(80, 20, 46, 13));
-        birthLabel = new QLabel(centralWidget);
+        birthLabel = new QLabel(tab);
         birthLabel->setObjectName(QString::fromUtf8("birthLabel"));
-        birthLabel->setGeometry(QRect(80, 60, 46, 13));
-        imgLabel = new QLabel(centralWidget);
-        imgLabel->setObjectName(QString::fromUtf8("imgLabel"));
-        imgLabel->setGeometry(QRect(80, 100, 46, 13));
-        fileEdit = new QPushButton(centralWidget);
-        fileEdit->setObjectName(QString::fromUtf8("fileEdit"));
-        fileEdit->setGeometry(QRect(150, 100, 111, 23));
-        birthdayEdit = new QDateEdit(centralWidget);
+        birthLabel->setGeometry(QRect(80, 80, 46, 13));
+        birthdayEdit = new QDateEdit(tab);
         birthdayEdit->setObjectName(QString::fromUtf8("birthdayEdit"));
-        birthdayEdit->setGeometry(QRect(150, 60, 110, 22));
-        myLabel = new QLabel(centralWidget);
-        myLabel->setObjectName(QString::fromUtf8("myLabel"));
-        myLabel->setGeometry(QRect(100, 140, 81, 71));
+        birthdayEdit->setGeometry(QRect(150, 80, 110, 22));
+        passwordLabel = new QLabel(tab);
+        passwordLabel->setObjectName(QString::fromUtf8("passwordLabel"));
+        passwordLabel->setGeometry(QRect(80, 50, 46, 13));
+        passwordEdit = new QLineEdit(tab);
+        passwordEdit->setObjectName(QString::fromUtf8("passwordEdit"));
+        passwordEdit->setGeometry(QRect(150, 50, 113, 20));
+        tabWidget->addTab(tab, QString());
+        tab_3 = new QWidget();
+        tab_3->setObjectName(QString::fromUtf8("tab_3"));
+        tabWidget->addTab(tab_3, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QString::fromUtf8("tab_2"));
+        tabWidget->addTab(tab_2, QString());
         TutorialClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(TutorialClass);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 343, 21));
+        menuBar->setGeometry(QRect(0, 0, 435, 21));
         TutorialClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(TutorialClass);
         mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
@@ -103,14 +128,20 @@ public:
         statusBar = new QStatusBar(TutorialClass);
         statusBar->setObjectName(QString::fromUtf8("statusBar"));
         TutorialClass->setStatusBar(statusBar);
-        QWidget::setTabOrder(nameEdit, fileEdit);
+        QWidget::setTabOrder(nameEdit, passwordEdit);
+        QWidget::setTabOrder(passwordEdit, birthdayEdit);
+        QWidget::setTabOrder(birthdayEdit, fileEdit);
         QWidget::setTabOrder(fileEdit, pushButton);
         QWidget::setTabOrder(pushButton, pushButton_2);
+        QWidget::setTabOrder(pushButton_2, tabWidget);
 
         retranslateUi(TutorialClass);
         QObject::connect(fileEdit, SIGNAL(clicked()), TutorialClass, SLOT(fileAction()));
         QObject::connect(pushButton_2, SIGNAL(clicked()), TutorialClass, SLOT(cancelAction()));
         QObject::connect(pushButton, SIGNAL(clicked()), TutorialClass, SLOT(saveAction()));
+
+        tabWidget->setCurrentIndex(0);
+
 
         QMetaObject::connectSlotsByName(TutorialClass);
     } // setupUi
@@ -118,13 +149,17 @@ public:
     void retranslateUi(QMainWindow *TutorialClass)
     {
         TutorialClass->setWindowTitle(QApplication::translate("TutorialClass", "Tutorial", 0, QApplication::UnicodeUTF8));
+        myLabel->setText(QString());
+        imgLabel->setText(QApplication::translate("TutorialClass", "Img", 0, QApplication::UnicodeUTF8));
         pushButton->setText(QApplication::translate("TutorialClass", "Save", 0, QApplication::UnicodeUTF8));
         pushButton_2->setText(QApplication::translate("TutorialClass", "Cancel", 0, QApplication::UnicodeUTF8));
+        fileEdit->setText(QApplication::translate("TutorialClass", "Browse ..", 0, QApplication::UnicodeUTF8));
         nameLabel->setText(QApplication::translate("TutorialClass", "Name", 0, QApplication::UnicodeUTF8));
         birthLabel->setText(QApplication::translate("TutorialClass", "Birthday", 0, QApplication::UnicodeUTF8));
-        imgLabel->setText(QApplication::translate("TutorialClass", "Img", 0, QApplication::UnicodeUTF8));
-        fileEdit->setText(QApplication::translate("TutorialClass", "Browse ..", 0, QApplication::UnicodeUTF8));
-        myLabel->setText(QString());
+        passwordLabel->setText(QApplication::translate("TutorialClass", "Password", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("TutorialClass", "Registration", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab_3), QApplication::translate("TutorialClass", "Page", 0, QApplication::UnicodeUTF8));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("TutorialClass", "Tab 2", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
--- Tutorial/tutorial.h
+++ Tutorial/tutorial.h
@@ -6,7 +6,9 @@
 #include <QByteArray>
 #include <QImage>
 #include <QPixmap>
+#include <QCryptographicHash>
 #include "ui_tutorial.h"
+#include <cstdlib>
 #include <string>
 #include "database.h"
 using std::string;
@@ -18,6 +20,9 @@ class Tutorial : public QMainWindow
 public:
 	Tutorial(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~Tutorial();
+	static const int thresholdImg    = 800;
+	static const int standardImgShow = 400;
+
 	QString getRestFromDelimeter(QString pathName,char delimeter)
 	{
 		pathName = pathName.trimmed();
@@ -63,12 +68,20 @@ private:
 			std::string temp = fn.toStdString();
 			char* strPath = (char*)temp.c_str();
 			loadImage(strPath,ui.myLabel);
-
+			// load big pic by tooltip
+			
 			QPixmap pix;
 			pix.load(fn);
 			int imgWidth  = pix.width()/2;
 			int imgHeight = pix.height()/2;
+			if(pix.width()>thresholdImg)
+			{
+				int scale = pix.width()/standardImgShow;
+				imgWidth  = pix.width()/scale;
+				imgHeight = pix.height()/scale;
+			}
 			ui.myLabel->setToolTip("<html><img src='"+fn+"' width='"+QString::number(imgWidth)+"' height='"+QString::number(imgHeight)+"' /></html>");
+			
 		}
 		void cancelAction()
 		{
@@ -80,41 +93,46 @@ private:
 			MYSQL_RES *res_set;
 			MYSQL_ROW row;
 
-			QString strName    = ui.nameEdit->text();
+			QString strName     = ui.nameEdit->text();
+			QString strPwdText  = ui.passwordEdit->text();
+			QByteArray tempPwd  = strPwdText.toUtf8();
+			QString strPwd      = QString(QCryptographicHash::hash((tempPwd),QCryptographicHash::Md5).toHex());
+
 			QString strBirthday = ui.birthdayEdit->date().toString(Qt::ISODate);
 			MYSQL* connection = database::connectByC();		
-			QString query = tr("INSERT INTO `qt_tracking`.`employee` (`id` ,`name` ,`birthday` ,`img`) VALUES (NULL , '");
-			query = query + strName +"','" + strBirthday+"','" + strFileName+"')" ;
-			
-			std::string query2 = query.toStdString();
-			const char* query1 = query2.c_str();
-			try
+			//get the last id from table
+			if(mysql_query(connection,"select MAX(id) from `qt_tracking`.`employee`") ==0)
 			{
-			//	QByteArray qTem = query.toUtf8();
-			//	char* query1    = qTem.data();
+				res_set = mysql_store_result(connection);
+				row = mysql_fetch_row(res_set);
+				int idInserted = atoi(row[0])+1;
+				char idInsert[5];
+				itoa(idInserted,idInsert,10);
+
+				QString path = QDir::currentPath();
+				QString name = getRestFromDelimeter(strFileName,'/');
+				QString typeFile = getRestFromDelimeter(name,'.');
+
+				QString query = tr("INSERT INTO `qt_tracking`.`employee` (`id` ,`name` , `password`, `birthday` , `img`) VALUES (NULL , '");
+				query = query + strName +"','"+strPwd +"','"+ strBirthday+"','" + idInsert+"."+typeFile+"')" ;
 				
-				int result = mysql_query(connection,query1);
-				if (result==0)
+				std::string query2 = query.toStdString();
+				const char* query1 = query2.c_str();
+				try
 				{
-					//get the last id from table
-					if(mysql_query(connection,"select MAX(id) from `qt_tracking`.`employee`") ==0)
+					int result = mysql_query(connection,query1);
+					if (result==0)
 					{
-						res_set = mysql_store_result(connection);
-						row = mysql_fetch_row(res_set);
-						QString path = QDir::currentPath();
-						QString name = getRestFromDelimeter(strFileName,'/');
-						QString typeFile = getRestFromDelimeter(name,'.');
-						QFile::copy(strFileName, QDir::currentPath() +"/Resources/img/"+row[0]+"."+typeFile);
+						QFile::copy(strFileName, QDir::currentPath() +"/Resources/img/"+idInsert+"."+typeFile);
 						return true;
-					}
-				}			
-				return false;				
+					}			
+					return false;				
+				}
+				catch(const ios_base::failure& e)
+				{
+					return false;
+				}
 			}
-			catch(const ios_base::failure& e)
-			{
-				return false;
-			}
-			
 			database::closeConnectionC(connection);
 			return true;
 		}
